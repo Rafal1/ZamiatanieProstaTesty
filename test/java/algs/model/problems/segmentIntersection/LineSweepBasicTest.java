7b71bd6f4a9b4f5d79b3fbf6266ae4c45cf0bd52
R <r@oo.pl>
2013-12-20 21:23:53 +0100

    whiteBox test for LinkedListLineSweep (results) + time

--- test/java/algs/model/problems/segmentIntersection/LineSweepBasicTest.java
+++ test/java/algs/model/problems/segmentIntersection/LineSweepBasicTest.java
@@ -5,7 +5,6 @@ import algs.model.IPoint;
 import algs.model.twod.TwoDLineSegment;
 import algs.model.twod.TwoDPoint;
 import org.junit.Test;
-//import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -20,37 +19,7 @@ public class LineSweepBasicTest {
     private LineSweep dba = new LineSweep(); // uwaga na wybor klasy!
 
     @Test
-    public void blackBox1() {
-        //brak punktow stycznych, ogólne sprawdzenie czy program działa
-        ArrayList<ILineSegment> als = new ArrayList<ILineSegment>();
-
-        //dodawanie punktow z ktorych powstana proste
-        TwoDLineSegment line1 = new TwoDLineSegment(new TwoDPoint(1.1, 5), new TwoDPoint(3, 8.3));
-
-        als.add(line1);
-
-        Hashtable<IPoint, ILineSegment[]> res = dba.intersections(als.iterator());
-
-        assertTrue(res.isEmpty());
-    }
-
-    @Test
-    public void blackBox2() {
-        //brak punktow stycznych kilku prostych
-        ArrayList<ILineSegment> als = new ArrayList<ILineSegment>();
-
-        TwoDLineSegment line1 = new TwoDLineSegment(new TwoDPoint(1, 5), new TwoDPoint(3, 8));
-        TwoDLineSegment line2 = new TwoDLineSegment(new TwoDPoint(-4, -2), new TwoDPoint(-1, -1));
-        als.add(line1);
-        als.add(line2);
-
-        Hashtable<IPoint, ILineSegment[]> res = dba.intersections(als.iterator());
-
-        assertTrue(res.isEmpty());
-    }
-
-    @Test
-    public void blackBox3() {
+    public void testBlackBox3() {
         //punkt wspólny
         ArrayList<ILineSegment> als = new ArrayList<ILineSegment>();
 
@@ -69,7 +38,7 @@ public class LineSweepBasicTest {
     }
 
     @Test
-    public void blackBox4() {
+    public void testBlackBox4() {
         //prostopadle i rownolegle
         //test nie przechodzi ze wzgledu na dokładność
         //-1.2000000000000002,0.6000000000000001 - końcówki!
@@ -89,14 +58,15 @@ public class LineSweepBasicTest {
 
         //spodziewany wynik
         IPoint expPoint = new TwoDPoint(0, 0);
-        IPoint expPoint1 = new TwoDPoint(-6 / 5, 3 / 5);
+        IPoint expPoint1 = new TwoDPoint(-1.2, 0.6);
+        System.out.print(res.containsKey(expPoint1));
 
         assertEquals(2, res.size());
         assertTrue(res.containsKey(expPoint) && res.containsKey(expPoint1));
     }
 
     @Test
-    public void blackBox5() {
+    public void testBlackBox5() {
         //pionowe i poziome linie
         ArrayList<ILineSegment> als = new ArrayList<ILineSegment>();
 
--- test/java/algs/model/problems/segmentIntersection/linkedlist/LineSweepTest.java
+++ test/java/algs/model/problems/segmentIntersection/linkedlist/LineSweepTest.java
@@ -1,13 +1,69 @@
 package algs.model.problems.segmentIntersection.linkedlist;
 
+import algs.model.ILineSegment;
+import algs.model.IPoint;
+import algs.model.twod.TwoDLineSegment;
+import algs.model.twod.TwoDPoint;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Rafal Zawadzki
  */
 public class LineSweepTest {
     @Test
-    public void testIntersections() throws Exception {
+    //   sprawdzenie zgodnosci wyników tych samych algorytmów z różną strukturą danych
+    public void testTheSameLineSweep() {
+        LineSweep alg = new LineSweep(); // LinkedList - lower performance
+        algs.model.problems.segmentIntersection.LineSweep alg1 = new algs.model.problems.segmentIntersection.LineSweep();
+        ArrayList<ILineSegment> lines = new ArrayList<ILineSegment>();
+
+        TwoDLineSegment line1 = new TwoDLineSegment(new TwoDPoint(0, 4), new TwoDPoint(10, 4));
+        TwoDLineSegment line2 = new TwoDLineSegment(new TwoDPoint(2, 0), new TwoDPoint(2, 10));
+        TwoDLineSegment line3 = new TwoDLineSegment(new TwoDPoint(6, 1), new TwoDPoint(6, 5));
+
+        lines.add(line1);
+        lines.add(line2);
+        lines.add(line3);
+
+        Hashtable<IPoint, ILineSegment[]> results = alg.intersections(lines.iterator());
+        Hashtable<IPoint, ILineSegment[]> results1 = alg1.intersections(lines.iterator());
+
+        IPoint expPoint = new TwoDPoint(2, 4);
+        IPoint expPoint1 = new TwoDPoint(6, 4);
+
+        assertEquals(2, results.size());
+        assertEquals(2, results1.size());
+        assertTrue(results.containsKey(expPoint) && results.containsKey(expPoint1));
+        assertTrue(results1.containsKey(expPoint) && results1.containsKey(expPoint1));
 
     }
+
+    @Test
+    //porównanie czasu działania algorytmów z różną strukturą danych
+    public void testTimeLineSweep() {
+        LineSweep alg = new LineSweep(); // LinkedList - lower performance
+        algs.model.problems.segmentIntersection.LineSweep alg1 = new algs.model.problems.segmentIntersection.LineSweep();
+        ArrayList<ILineSegment> lines = new ArrayList<ILineSegment>();
+
+        TwoDLineSegment line1 = new TwoDLineSegment(new TwoDPoint(-3, -6), new TwoDPoint(8, 16));
+        TwoDLineSegment line2 = new TwoDLineSegment(new TwoDPoint(-3, -3), new TwoDPoint(2, 7));
+        TwoDLineSegment line3 = new TwoDLineSegment(new TwoDPoint(-4, 2), new TwoDPoint(5, -2.5));
+
+        lines.add(line1);
+        lines.add(line2);
+        lines.add(line3);
+
+        Hashtable<IPoint, ILineSegment[]> results = alg.intersections(lines.iterator());
+        Hashtable<IPoint, ILineSegment[]> results1 = alg1.intersections(lines.iterator());
+
+        System.out.println("Czas wykonania alg. ze strukturą LinkedListLineState: " + alg.time() + ", czas alg. ze strukturą LineState: " + alg1.time());
+        assertTrue(alg.time() > alg1.time());
+    }
+
 }
